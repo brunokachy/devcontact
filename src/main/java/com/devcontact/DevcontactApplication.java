ec4f673381f2a522532a58f8ed616f2c13a1f92a
Bruno <bruno.okafor@gmail.com>
2019-01-08 12:31:50 +0100

    New Project Created

---
+++ src/main/java/com/devcontact/DevcontactApplication.java
@@ -0,0 +1,14 @@
+package com.devcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcontactApplication.class, args);
+	}
+
+}
+
